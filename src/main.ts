986cfd296fd3bef20da84c2af4c159dcf439fad7
Leandro Tercette <tal1wrn@bosch.com>
2024-10-25 20:22:28 -0300

    feat: main.ts file updated with routes, browserAnimation and routes config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N, pt_BR } from 'ng-zorro-antd/i18n';
 import { AppComponent } from './app/app.component';
+import { ItemListComponent } from './app/item-list/item-list.component';
+import { ItemFormComponent } from './app/item-form/item-form.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: 'list', component: ItemListComponent },
+  { path: 'form', component: ItemFormComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule),
+    { provide: NZ_I18N, useValue: pt_BR }
+  ],
+});
